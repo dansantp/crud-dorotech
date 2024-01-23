6d849c52109434a5a038cc7bd488438c2748ed48
Daniel Pereira <santospereirad73@gmail.com>
2024-01-23 17:46:41 -0300

    Commit inicial - Criação de um CRUD.

---
+++ src/main/java/com/dorotech/crud/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.dorotech.crud.controller;
+
+import com.dorotech.crud.domain.Product;
+import com.dorotech.crud.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable String id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable String id, @RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable String id) {
+        productRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/dorotech/cruddorotech/CrudDorotechApplication.java
@@ -0,0 +1,13 @@
+package com.dorotech.cruddorotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDorotechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( CrudDorotechApplication.class, args );
+    }
+
+}
